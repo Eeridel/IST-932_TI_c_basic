1e3baf2576d3374a92561f46e1e31820d984a715
Eeridel <Eeridel@users.noreply.github.com>
2020-03-26 14:57:23 +0300

    Update 26.03.matrix.c
    + ex. 3

--- 26.03.matrix.c
+++ 26.03.matrix.c
@@ -3,11 +3,11 @@
 #include <limits.h>
 #define M 10
 #define N 10
-#define L 100
+#define L 10
 
 int main()
 {
-    int i, j, k, n, temp_min, temp_max, a[M][N], min_N[N], min_M[M], max_M[M];
+    int i, j, k, temp_min, temp_max, a[M][N], min_N[N], min_M[M], max_M[M], com_M[M], temp_com, com_iM;
 
     srand(time(NULL));
 
@@ -76,6 +76,32 @@ int main()
         printf("\n");
     }
 
+    printf("\n3.\n");
+
+    for(k = 0; k < M; k++)
+    {
+        for(i = 0, com_M[k] = 1; i < N; i++)
+        {
+            for(j = i + 1, temp_com = 1; j < N; j++) if(a[k][i] == a[k][j]) temp_com++;
+            if(com_M[k] < temp_com)
+            {
+                com_M[k] = temp_com;
+            }
+        }
+    }
+
+    for(i = 0; i < N; i++) printf("\n    com_M[%d] = %d", i, com_M[i]);
+
+    for(i = 0, temp_max = INT_MIN; i < M; i++)
+    {
+        if(com_M[i] > temp_max)
+        {
+            temp_max = com_M[i];
+            com_iM = i;
+        }
+    }
+
+    printf("\n\nSequence number of the line with the largest number of common: %d\n", com_iM);
 
 
 
